f2f3c900fdd0e675df3803f4719416cf72fa101a
Pranay Patle <pranayhpatle@gmail.com>
2024-02-16 11:00:53 +0530

    feat();- Added simple skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import Header from "./components/shared/header";
 import { useDataStore } from "./lib/state";
 import { useEffect } from "react";
 import TremorDashboard from "./components/shared/tremor-dashboard";
+import { Skeleton } from "./components/ui/skeleton";
 
 const App = () => {
-  const { setDataManual } = useDataStore();
+  const { setDataManual, isLoading } = useDataStore();
   useEffect(() => {
     setDataManual();
   }, []);
@@ -19,7 +20,23 @@ const App = () => {
           <Header />
         </div>
         <div className=" lg:mx-10 my-4 gap-10 mx-4 ">
-          <TremorDashboard />
+          {isLoading ? (
+            <>
+              <div className="flex-col gap-4 space-y-4  ">
+                <div className="flex flex-col md:flex-row  gap-4 ">
+                  <Skeleton className="h-[50vh] md:w-[75%] w-[100%] rounded-xl" />
+                  <Skeleton className="h-[50vh] md:w-[25%] w-[100%] rounded-xl" />
+                </div>
+                <div className="flex flex-col md:flex-row  gap-4 ">
+                  <Skeleton className="h-[50vh] md:w-[25%] w-[100%] rounded-xl" />
+                  <Skeleton className="h-[50vh] md:w-[75%] w-[100%] rounded-xl" />
+                </div>
+             
+              </div>
+            </>
+          ) : (
+            <TremorDashboard />
+          )}
         </div>
       </main>
     </ThemeProvider>
